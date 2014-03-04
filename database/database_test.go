34c0f04b60bcc0e04f6458cc5eb79374bda70344
Jean-Claude Wippler <jc@wippler.nl>
2014-03-04 22:39:43 +0100

    simplify

--- database/database_test.go
+++ database/database_test.go
@@ -8,81 +8,64 @@ import (
 	_ "github.com/jcw/flow/workers"
 )
 
-const DB_PATH = "./test-db"
+var dbPath = os.TempDir() + "flow-test-db"
 
 func init() {
-	os.RemoveAll(DB_PATH)
+	println(dbPath)
 }
 
-// makeDb := func() flow.Worker {
-// 	g := flow.NewGroup()
-// 	g.Add("d", "LevelDB")
-// 	g.Add("p", "Printer")
-// 	g.Connect("d.Out", "p.In", 0)
-// 	g.Map("Get", "d.Get")
-// 	g.Map("Put", "d.Put")
-// 	g.Map("Keys", "d.Keys")
-// 	g.Set("db.Name", DB_PATH)
-// 	return g
-// }
-// g.AddWorker("db", makeDb())
-
 func ExampleLevelDB() {
-	// TODO: clumsy, use workgroups and/or external channels
+	// TODO: clumsy, use external channels
+
+	makeDb := func() flow.Worker {
+		g := flow.NewGroup()
+		g.Add("d", "LevelDB")
+		g.Add("p", "Printer")
+		g.Connect("d.Out", "p.In", 0)
+		g.Map("Get", "d.Get")
+		g.Map("Put", "d.Put")
+		g.Map("Keys", "d.Keys")
+		g.Set("d.Name", dbPath)
+		return g
+	}
 
 	g := flow.NewGroup()
-	g.Add("db", "LevelDB")
-	g.Set("db.Name", DB_PATH)
+	g.AddWorker("db", makeDb())
 	g.Set("db.Put", []string{"a/b", "123"})
 	g.Run()
 
 	g = flow.NewGroup()
-	g.Add("db", "LevelDB")
-	g.Set("db.Name", DB_PATH)
+	g.AddWorker("db", makeDb())
 	g.Set("db.Put", []string{"a/c", "456"})
 	g.Run()
 
 	g = flow.NewGroup()
-	g.Add("db", "LevelDB")
-	g.Add("p", "Printer")
-	g.Connect("db.Out", "p.In", 0)
-	g.Set("db.Name", DB_PATH)
+	g.AddWorker("db", makeDb())
 	g.Set("db.Get", "a/b")
 	g.Run()
 
 	g = flow.NewGroup()
-	g.Add("db", "LevelDB")
-	g.Add("p", "Printer")
-	g.Connect("db.Out", "p.In", 0)
-	g.Set("db.Name", DB_PATH)
+	g.AddWorker("db", makeDb())
 	g.Set("db.Keys", "a/")
 	g.Run()
 
 	g = flow.NewGroup()
-	g.Add("db", "LevelDB")
-	g.Set("db.Name", DB_PATH)
+	g.AddWorker("db", makeDb())
 	g.Set("db.Put", []string{"a/b"})
 	g.Run()
 
 	g = flow.NewGroup()
-	g.Add("db", "LevelDB")
-	g.Add("p", "Printer")
-	g.Connect("db.Out", "p.In", 0)
-	g.Set("db.Name", DB_PATH)
+	g.AddWorker("db", makeDb())
 	g.Set("db.Get", "a/b")
 	g.Run()
 
 	g = flow.NewGroup()
-	g.Add("db", "LevelDB")
-	g.Add("p", "Printer")
-	g.Connect("db.Out", "p.In", 0)
-	g.Set("db.Name", DB_PATH)
+	g.AddWorker("db", makeDb())
 	g.Set("db.Keys", "a/")
 	g.Run()
 
 	g = flow.NewGroup()
-	g.Add("db", "LevelDB")
-	g.Set("db.Name", DB_PATH)
+	g.AddWorker("db", makeDb())
 	g.Set("db.Put", []string{"a/c"})
 	g.Run()
 	// Output:
@@ -95,6 +78,6 @@ func ExampleLevelDB() {
 func TestDatabase(t *testing.T) {
 	g := flow.NewGroup()
 	g.Add("db", "LevelDB")
-	g.Set("db.Name", DB_PATH)
+	g.Set("db.Name", dbPath)
 	g.Run()
 }
