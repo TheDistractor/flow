5eb057d2ce8cd3388037f3b546ca90e992f6f793
Jean-Claude Wippler <jc@wippler.nl>
2014-03-04 17:31:07 +0100

    simplify

--- javascript/javascript_test.go
+++ javascript/javascript_test.go
@@ -2,14 +2,12 @@ package javascript
 
 import (
 	"github.com/jcw/flow/flow"
-	_ "github.com/jcw/flow/workers"
+	_ "github.com/jcw/flow/javascript"
 )
 
 func ExampleJavaScript() {
 	g := flow.NewGroup()
 	g.Add("js", "JavaScript")
-	g.Add("p", "Printer")
-	g.Connect("js.Out", "p.In", 0)
 	g.Request(`console.log("Hello from Otto!");`, "js.Cmd")
 	g.Run()
 	// Output:
