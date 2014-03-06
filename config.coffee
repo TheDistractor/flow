f354cdcf786f6b6d837920c8bcea05c90060a87b
Jean-Claude Wippler <jc@wippler.nl>
2014-03-06 11:53:17 +0100

    simulated replay works

--- config.coffee
+++ config.coffee
@@ -1,13 +1,18 @@
 group = 
   workers: [
-    { name: "si", type: "SerialIn" }
+    { name: "lr", type: "LogReader" }
+    { name: "rf", type: "Pipe" } # used to inject an "[RF12demo...]" line
+    { name: "w1", type: "LogReplayer" }
+    # { name: "w1", type: "SerialIn" }
     { name: "ts", type: "TimeStamp" }
     { name: "st", type: "SketchType" }
     { name: "nm", type: "Decoder-NodeMap" }
     { name: "p", type: "Printer" }
   ]
   connections: [
-    { from: "si.Out", to: "ts.In" }
+    { from: "lr.Out", to: "w1.In" }
+    { from: "rf.Out", to: "ts.In" }
+    { from: "w1.Out", to: "ts.In" }
     { from: "ts.Out", to: "st.In" }
     { from: "st.Out", to: "nm.In" }
     { from: "nm.Out", to: "p.In" }
@@ -23,13 +28,16 @@ group =
     { data: "RFg5i11 roomNode",  to: "nm.Info" }
     { data: "RFg5i12 roomNode",  to: "nm.Info" }
     { data: "RFg5i13 roomNode",  to: "nm.Info" }
+    { data: "RFg5i14 otRelay",   to: "nm.Info" }
     { data: "RFg5i15 smaRelay",  to: "nm.Info" }
     { data: "RFg5i18 p1scanner", to: "nm.Info" }
     { data: "RFg5i19 ookRelay",  to: "nm.Info" }
     { data: "RFg5i23 roomNode",  to: "nm.Info" }
     { data: "RFg5i24 roomNode",  to: "nm.Info" }
     
-    { data: "/dev/tty.usbserial-A901ROSM", to: "si.Port" }
+    { data: "[RF12demo.10] _ i31* g5 @ 868 MHz", to: "rf.In" }
+    { data: "./rfdata/20121130.txt.gz", to: "lr.Name" }
+    # { data: "/dev/tty.usbserial-A901ROSM", to: "w1.Port" }
   ]
 
 console.log JSON.stringify group, null, 4
--- rfdata/replay.go
+++ rfdata/replay.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path"
 	"regexp"
+	"sort"
 	"time"
 
 	"github.com/jcw/flow/flow"
@@ -14,6 +15,7 @@ import (
 
 func init() {
 	flow.Registry["LogReader"] = func() flow.Worker { return &LogReader{} }
+	flow.Registry["LogReplayer"] = func() flow.Worker { return &LogReplayer{} }
 }
 
 // LogReader opens a (possibly compressed) log file and sends out its entries.
@@ -49,7 +51,7 @@ func (w *LogReader) Run() {
 		}
 		yr, mo, dy := day.Date()
 		lastDev := ""
-		
+
 		scanner := bufio.NewScanner(file)
 		for scanner.Scan() {
 			m := re.FindStringSubmatch(scanner.Text())
@@ -65,5 +67,48 @@ func (w *LogReader) Run() {
 			}
 			w.Out.Send(m[3])
 		}
+	}
+}
+
+// A LogReplayer generates new entries from existing ones in simulated time.
+type LogReplayer struct {
+	flow.Work
+	In  flow.Input
+	Out flow.Output
+}
+
+// Start reading log entries, and replay them as if they happened today.
+func (w *LogReplayer) Run() {
+	const MS_PER_DAY = 86400000
+	// collect all log entries into two arrays (easier to search that way)
+	var times []int
+	var texts []string
+	var t time.Time
+	for m := range w.In {
+		switch v := m.(type) {
+		case time.Time:
+			t = v
+		case string:
+			times = append(times, int((t.UnixNano()/1000000)%MS_PER_DAY))
+			texts = append(texts, v)
+		}
+	}
+	// add a final entry to wrap back to the beginning on each new day
+	// this avoids having to special-case searching past the last entry
+	times = append(times, times[0]+MS_PER_DAY)
+	for {
+		// find best candidate using binary search
+		ms := int((time.Now().UnixNano() / 1000000) % MS_PER_DAY)
+		i := sort.SearchInts(times, ms)
+		// sleep until it's time for that next entry
+		time.Sleep(time.Duration(times[i]-ms) * time.Millisecond)
+		// wrap around at the end of the day
+		if i >= len(texts) {
+			i = 0
+		}
+		// replay the string to the output port
+		w.Out.Send(texts[i])
+		// advance time so that the next search will find a different entry
+		time.Sleep(time.Millisecond)
 	}
 }
