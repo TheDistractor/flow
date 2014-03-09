8de2d19f2d5e3719b1637a837890504ad810d95e
Jean-Claude Wippler <jc@wippler.nl>
2014-03-09 19:16:06 +0100

    add websocket dispatcher, not tested yet

--- network/http.go
+++ network/http.go
@@ -1,9 +1,12 @@
 package network
 
 import (
+	"io"
 	"net/http"
+	"strings"
 	"time"
 
+	"code.google.com/p/go.net/websocket"
 	"github.com/jcw/flow/flow"
 )
 
@@ -11,6 +14,7 @@ func init() {
 	flow.Registry["HttpServer"] = func() flow.Worker { return &HttpServer{} }
 }
 
+// HttpServer is a worker which sets up an HTTP server.
 type HttpServer struct {
 	flow.Work
 	Handlers flow.Input
@@ -28,16 +32,14 @@ func (fh *flowHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	fh.h.ServeHTTP(w, req)
 }
 
+// Set up the handlers, then start the server and start processing requests.
 func (w *HttpServer) Run() {
 	mux := http.NewServeMux() // don't use default to allow multiple instances
 	for m := range w.Handlers {
 		tag := m.(flow.Tag)
 		switch v := tag.Val.(type) {
 		case string:
-			h := http.FileServer(http.Dir(v))
-			if v != "/" {
-				h = http.StripPrefix(tag.Tag, h)
-			}
+			h := createHandler(tag.Tag, v)
 			mux.Handle(tag.Tag, &flowHandler{h, w})
 		case http.Handler:
 			mux.Handle(tag.Tag, &flowHandler{v, w})
@@ -51,4 +53,76 @@ func (w *HttpServer) Run() {
 	// TODO: this is a hack to make sure the server is ready
 	// better would be to interlock the goroutine with the listener being ready
 	time.Sleep(10 * time.Millisecond)
+}
+
+func createHandler(tag, s string) http.Handler {
+	// TODO: hook worker in as HTTP handler
+	// if _, ok := flow.Registry[s]; ok {
+	// 	return http.Handler(reqHandler)
+	// }
+	if s == "<websocket>" {
+		return websocket.Handler(wsHandler)
+	}
+	if strings.ContainsAny(s, "./") {
+		h := http.FileServer(http.Dir(s))
+		if s != "/" {
+			h = http.StripPrefix(tag, h)
+		}
+		return h
+	}
+	panic("cannot create handler for: " + s)
+}
+
+func wsHandler(ws *websocket.Conn) {
+	defer ws.Close()
+
+	tag := ws.Request().Header.Get("Sec-Websocket-Protocol")
+	if tag == "" {
+		tag = "default"
+	}
+
+	g := flow.NewGroup()
+	g.AddWorker("head", &wsHead{ws: ws})
+	g.Add("ws", "WebSocket-"+tag)
+	g.AddWorker("tail", &wsTail{ws: ws})
+	g.Connect("head.Out", "ws.In", 0)
+	g.Connect("ws.Out", "tail.In", 0)
+	g.Run()
+}
+
+type wsHead struct {
+	flow.Work
+	Out flow.Output
+
+	ws *websocket.Conn
+}
+
+func (w *wsHead) Run() {
+	for {
+		var msg interface{}
+		err := websocket.JSON.Receive(w.ws, &msg)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		w.Out.Send(msg)
+	}
+}
+
+type wsTail struct {
+	flow.Work
+	In flow.Input
+
+	ws *websocket.Conn
+}
+
+func (w *wsTail) Run() {
+	for m := range w.In {
+		err := websocket.JSON.Send(w.ws, m)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
