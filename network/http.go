f93cffce83c4298c848ee5214d9e6578188b18a3
Jean-Claude Wippler <jc@wippler.nl>
2014-03-09 02:10:39 +0100

    cleanup

--- network/http.go
+++ network/http.go
@@ -17,12 +17,12 @@ type HttpServer struct {
 	Out      flow.Output
 }
 
-type FlowHandler struct {
+type flowHandler struct {
 	h http.Handler
 	s *HttpServer
 }
 
-func (fh *FlowHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (fh *flowHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	fh.s.Out.Send(req.URL)
 	fh.h.ServeHTTP(w, req)
 }
@@ -31,7 +31,7 @@ func (w *HttpServer) Run() {
 	mux := http.NewServeMux() // don't use default to allow multiple instances
 	for m := range w.Handlers {
 		tag := m.(flow.Tag)
-		mux.Handle(tag.Tag, &FlowHandler{tag.Val.(http.Handler), w})
+		mux.Handle(tag.Tag, &flowHandler{tag.Val.(http.Handler), w})
 	}
 	m := <-w.Start
 	go func() {
